35f0884189f1aa52c3e6faf910da1e24c55498ce
Guilherme Henrique Martin M. Pereira <guilherme.pereira@detran.dtft>
2019-07-05 10:32:26 -0300

    SW
    Ajustes para o ServiceWorker funcionar.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 // Check if browser supports service worker
 if('serviceWorker' in navigator){
     // If does
-    navigator.serviceWorker.register('/lifetree/sw.js')
+    navigator.serviceWorker.register('/site/sw.js')
         .then((reg) => console.log('[Service worker] registered', reg))
         .catch((err) => console.log('[Service worker] not registered', err))
 }
